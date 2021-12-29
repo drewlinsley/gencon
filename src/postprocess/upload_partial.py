9d65e8fc8861de69485a342b40e59aec2716b24a
pathtracker-code <pathtrackercode@gmail.com>
2021-12-29 12:48:29 -0500

    Fixed upload to native res.

--- src/postprocess/upload_partial.py
+++ src/postprocess/upload_partial.py
@@ -5,19 +5,36 @@ from webknossos.dataset import COLOR_CATEGORY, SEGMENTATION_CATEGORY
 from webknossos.dataset.properties import LayerViewConfiguration
 from config import Config
 from skimage.segmentation import relabel_sequential as rs
+from skimage.transform import resize
 
 
 # Create the dataset
 config = Config()
 x, y, z = 0, 0, 0
 scale = (5, 5, 50)
-seg_dtype = np.int32
-with wk.webknossos_context(url="http://localhost:9000", token=config.token):
+seg_dtype = np.uint32
+mem_dtype = np.uint8
+ds_input = "/cifs/data/tserre/CLPS_Serre_Lab/connectomics/cubed_mag1/pbtest/wong_1"
+image_shape = [240, 4740, 4740]
+with wk.webknossos_context(url="https://webknossos.org", token=config.token):
     img_mem = np.load("/cifs/data/tserre_lrs/projects/prj_connectomics/wong/mag1_membranes/x{}/y{}/z{}/110629_k0725_mag1_x{}_y{}_z{}.npy".format(x, y, z, x, y, z))
-    img = img_mem[..., 0]
-    mem = img_mem[..., 1]
+    # img = img_mem[..., 0]
+    mem = img_mem[..., 1].astype(mem_dtype).transpose(1, 2, 0)
     seg = np.load("/cifs/data/tserre_lrs/projects/prj_connectomics/wong/mag1_segs/x{}/y{}/z{}/110629_k0725_mag1_x{}_y{}_z{}.npy".format(x, y, z, x, y, z))
-    seg = rs(seg)[0].astype(seg_dtype).transpose(2, 0, 1)
+    seg = rs(seg)[0].astype(seg_dtype)
+
+    # Get native resolution
+    ds_native = wk.Dataset(ds_input, scale=[5, 5, 50], exist_ok=True)
+    layer = ds_native.get_layer("color")
+    mag1 = layer.get_mag("1")
+    print("Extracting images")
+    img = mag1.read((x, y, z), image_shape[::-1])[0]
+
+    # Resize images
+    res_shape = np.asarray(image_shape)
+    print("Resizing mems and segs")
+    mem = resize(mem, res_shape[::-1][:-1], anti_aliasing=False, preserve_range=True, order=0)
+    seg = resize(seg, res_shape[::-1][:-1], anti_aliasing=False, preserve_range=True, order=0)
 
     # Choose a name for our dataset
     time_str = strftime("%Y-%m-%d_%H-%M-%S", gmtime())
@@ -36,13 +53,13 @@ with wk.webknossos_context(url="http://localhost:9000", token=config.token):
     layer_images.downsample()
 
     # Add membranes
-    mem_config = LayerViewConfiguration(color=[255, 0, 0])
+    # mem_config = LayerViewConfiguration(color=[255, 0, 0])
     layer_membranes = ds.add_layer(
         "membranes",
         SEGMENTATION_CATEGORY,
-        np.uint8,
+        mem_dtype,
         num_channels=1,
-        default_view_configuration=mem_config,
+        # default_view_configuration=mem_config,
         largest_segment_id=np.iinfo(np.uint8).max
     )
     layer_membranes.add_mag(1, compress=True).write(mem)
@@ -55,7 +72,7 @@ with wk.webknossos_context(url="http://localhost:9000", token=config.token):
         seg_dtype,
         num_channels=1,
         # compressed=True,
-        largest_segment_id=8000,  # seg.max()
+        largest_segment_id=seg.max(),
     )  # color? maxval? channels?
     layer_segmentations.add_mag(1, compress=True).write(seg)
     layer_segmentations.downsample()
